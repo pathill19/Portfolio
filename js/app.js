7f7662b8953f1d5c76760b8444a460fc0acddd3b
pathill19 <43425650+pathill19@users.noreply.github.com>
2019-02-19 19:20:10 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-VANTA.TOPOLOGY({
+// From vantajs.com
+VANTA.FOG({
   el: ".hero",
-  color: 0xff1e95,
-  backgroundColor: 0xffffff
+  highlightColor: 0x3bcf5,
+  midtoneColor: 0x86d2f2,
+  lowlightColor: 0x6344f5,
+  blurFactor: 0.40,
+  speed: 0.50,
+  zoom: 0.70
 })
